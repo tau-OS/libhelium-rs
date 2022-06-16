7abf2162fede7c9f3849025f2eb85d379c16675a
ItsJamie9494 <hello@jamiethalacker.dev>
2022-06-16 02:20:36 -0700

    Pain peko

--- src/subclass/mod.rs
+++ src/subclass/mod.rs
@@ -1,6 +1,10 @@
 pub mod application;
+pub mod bin;
+pub mod content_list;
 
 pub mod prelude {
+    pub use super::content_list::ContentListImpl;
+    pub use super::bin::BinImpl;
     pub use gio::subclass::prelude::*;
     pub use glib::subclass::prelude::*;
 }
