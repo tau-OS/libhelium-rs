2e6066273448b703807efd890ef01c80ef796c7e
Jamie Murphy <hello@jamiethalacker.dev>
2022-06-26 00:44:21 -0700

    Add MiniContentBlock for subclass

--- src/subclass/mod.rs
+++ src/subclass/mod.rs
@@ -3,6 +3,7 @@ pub mod bin;
 pub mod content_list;
 pub mod window;
 pub mod application_window;
+pub mod mini_content_block;
 
 pub mod prelude {
     pub use super::content_list::ContentListImpl;
@@ -10,6 +11,7 @@ pub mod prelude {
     pub use super::application::HeApplicationImpl;
     pub use super::window::HeWindowImpl;
     pub use super::application_window::HeApplicationWindowImpl;
+    pub use super::mini_content_block::MiniContentBlockImpl;
     pub use gio::subclass::prelude::*;
     pub use glib::subclass::prelude::*;
 }
